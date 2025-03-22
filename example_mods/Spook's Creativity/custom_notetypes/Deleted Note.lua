5ce184a6a12fcc6ab5cc199959689e841e8f87f2
Spook0w0 <spookupt@gmail.com>
2025-03-21 21:27:35 -0700

    depression update

---
+++ example_mods/Spook's Creativity/custom_notetypes/Deleted Note.lua
@@ -0,0 +1,17 @@
+function onCreate()
+    for i = 0, getProperty('unspawnNotes.length') - 1 do
+        if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Deleted Note' then
+            setPropertyFromGroup('unspawnNotes', i, 'texture', 'Deleted Note')
+            setPropertyFromGroup('unspawnNotes', i, 'hitHealth', -0.1)
+        end
+    end
+end
+
+function goodNoteHit(id, direction, noteType, isSustainNote)
+    if noteType == 'Deleted Note' then
+        playSound('staticShock', 1)
+        cameraShake('camHUD', 0.01, 0.2)
+        cameraShake('camDiscord', 0.01, 0.2)
+        triggerEvent('Screen Shake', '0.01, 0.2', '')
+    end
+end
