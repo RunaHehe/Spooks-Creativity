76ee35363f2693b09edb709595a57fce14be6f35
VideoBot <lander.meersman@gmail.com>
2025-04-19 15:12:00 +0200

    removed Depression modchart from source

--- assets/preload/data/depression/customMods/rotate.hx
+++
@@ -1,8 +0,0 @@
-//
-function initMod(mod)
-{
-	mod.noteMath = function(noteData:NotePositionData, lane:Int, curPos:Float, pf:Int)
-	{
-		
-	}
-}
--- assets/preload/data/depression/customMods/wiggle.hx
+++
@@ -1,8 +0,0 @@
-//
-function initMod(mod)
-{
-	mod.noteMath = function (noteData:NotePositionData, lane:Int, curPos:Float, pf:Int)
-	{
-		noteData.x += FlxMath.fastSin(0 + (curPos * 0.004)) * (NoteMovement.arrowSizes[lane] * 1 * mod.currentValue * 0.5);
-	}
-}
