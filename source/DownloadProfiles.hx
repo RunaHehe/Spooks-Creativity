20b5888984e90451b358bc0ad6a8c0246bded0b9
Spook0w0 <spookupt@gmail.com>
2025-04-22 01:01:05 -0700

    help

---
+++ source/DownloadProfiles.hx
@@ -0,0 +1,99 @@
+import funkin.backend.utils.DiscordUtil.DUser;
+import haxe.Json;
+import haxe.Http;
+import openfl.display.BitmapData;
+import openfl.display.PNGEncoderOptions;
+import openfl.utils.ByteArray;
+import sys.FileSystem;
+import sys.io.File;
+
+class DownloadProfiles
+{
+	public static function downloadAsync(userData:Array<Dynamic>, forceDownload:Bool = false)
+	{
+		trace('Downloading avatars...');
+
+		var folderPath:String = "assets/images/profiles";
+		if (!FileSystem.exists(folderPath))
+			FileSystem.createDirectory(folderPath);
+
+		for (data in userData)
+		{
+			if (data[0] == '0') continue;
+
+			var imagePath:String = getImagePath(data[1]);
+
+			if (!FileSystem.exists(imagePath) || forceDownload)
+			{
+				downloadPFPAsync(data[0], data[1], data[2]);
+			}
+		}
+	}
+
+	static function downloadPFPAsync(userId:String, fileName:String, spriteName:String)
+	{
+		var userInfoUrl = "https://discordlookup.mesalytic.moe/v1/user/" + userId;
+		var http = new Http(userInfoUrl);
+
+		http.onData = function(response:String)
+		{
+			try
+			{
+				var userDataParsed = Json.parse(response).raw;
+				var avatarId:String = userDataParsed.avatar;
+				var avatarURL = 'https://cdn.discordapp.com/avatars/$userId/$avatarId.png?size=128';
+
+				// download avatar :3
+				var avatarHttp = new Http(avatarURL);
+
+				avatarHttp.onBytes = function(bytes:haxe.io.Bytes)
+				{
+					try
+					{
+						var byteArray = ByteArray.fromBytes(bytes);
+						var bitmapData = BitmapData.fromBytes(byteArray);
+
+						saveBitmap(bitmapData, fileName);
+						trace('+ Installed profile | ' + fileName + ".png");
+					}
+					catch (err)
+					{
+						trace('[Bitmap Error] Failed to decode avatar image: ' + err);
+					}
+				}
+
+				avatarHttp.onError = function(err)
+				{
+					trace('[Avatar Error] Couldn\'t fetch avatar image: ' + err);
+				}
+
+				avatarHttp.request(); // start avatar download
+			}
+			catch (e)
+			{
+				trace('[Download Error] Failed to parse user data for ' + userId + ': ' + e);
+			}
+		}
+
+		http.onError = function(error:String)
+		{
+			trace('[Download Error] Couldn\'t fetch user info: ' + error);
+		}
+
+		http.request(); // start user info request
+	}
+
+
+
+	static function saveBitmap(image:BitmapData, fileName:String)
+	{
+		var path = "assets/images/profiles" + fileName + ".png";
+		var bytes:ByteArray = image.encode(image.rect, new PNGEncoderOptions());
+		File.saveBytes(path, bytes);
+	}
+
+	static function getImagePath(fileName:String):String
+	{
+		return "assets/images/profiles" + fileName + ".png";
+	}
+}
