8e3d5f1a872ca4cc5f04cd76f519d2bd670dbc48
Spook0w0 <spookupt@gmail.com>
2025-05-04 02:15:36 -0700

    d

---
+++ assets/preload/data/moderator/longnoteblocker.lua
@@ -0,0 +1,30 @@
+-- these two are in seperate scripts because im honestly not dealing with the bugs :3
+function onCreatePost()
+    createLongBlocker()
+end
+
+function createLongBlocker()
+    for i = 1, 5 do
+        local tag2 = 'longBlock'..i
+        makeLuaSprite(tag2, 'mod/Timeout Note Long Blocker', 1300 , 20 + ((i-1) * 150))
+        scaleObject(tag2, 0.75, 0.75)
+        setObjectCamera(tag2, 'hud')
+        addLuaSprite(tag2)
+        setObjectOrder(tag2, getObjectOrder('notes')+10)
+    end
+end
+
+function onStepEvent(curStep)
+    if (curStep >= 1411 and curStep < 1526) and curStep % 4 == 0 then
+        local tag2 = 'longBlock'..((curStep%(4*5))/4) + 1
+        setProperty(tag2..'.angle', 20)
+        doTweenX(tag2, tag2, 650, crochet/500, 'quadOut')
+        doTweenAngle(((curStep%(4*6))/4)..'hehehehe', tag2, 0, crochet/500, 'quadOut')
+    end
+end
+
+function onTweenCompleted(tag2)
+    if string.find(tag2, 'longBlock') and not string.find(tag2, 'outt') then
+        doTweenX(tag2..'outt', tag2, 1300, crochet/1000, 'quadIn')
+    end
+end
---
+++ assets/preload/data/moderator/noteblocker.lua
@@ -0,0 +1,29 @@
+function onCreatePost()
+    createNoteBlocker()
+end
+
+function createNoteBlocker()
+    for i = 1, 5 do
+        local tag = 'blockerDis'..i
+        makeLuaSprite(tag, 'mod/Timeout Note Blocker', 1300 , 20 + ((i-1) * 150))
+        scaleObject(tag, 0.75, 0.75)
+        setObjectCamera(tag, 'hud')
+        addLuaSprite(tag)
+        setObjectOrder(tag, getObjectOrder('notes')+10)
+    end
+end
+
+function onStepEvent(curStep)
+    if (curStep >= 1280 and curStep < 1408) and curStep % 4 == 0 then
+        local tag = 'blockerDis'..((curStep%(4*5))/4) + 1
+        setProperty(tag..'.angle', 20)
+        doTweenX(tag, tag, 800, crochet/500, 'quadOut')
+        doTweenAngle(((curStep%(4*6))/4)..'hehehehe', tag, 0, crochet/500, 'quadOut')
+    end
+end
+
+function onTweenCompleted(tag)
+    if string.find(tag, 'blockerDis') and not string.find(tag, 'outt') then
+        doTweenX(tag..'outt', tag, 1300, crochet/1000, 'quadIn')
+    end
+end
