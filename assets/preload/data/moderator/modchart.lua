cdbc0a35e6aacf5ecc2dd58e06ba7b9973aa13a1
VideoBot <lander.meersman@gmail.com>
2025-06-01 13:33:50 +0200

    improved moderator bc i can

--- assets/preload/data/moderator/modchart.lua
+++ assets/preload/data/moderator/modchart.lua
@@ -6,9 +6,11 @@ function onCreatePost()
     setMod("opY", -165)
     setMod("bfY", -165)
     setMod("opSwap", 0.5)
-    setMod("bumpy", 1)
-    setSubMod("bumpy", "speed", 1.2)
-    setSubMod("hidden", "offset", 4)
+    if HardMode then
+        setMod("bumpy", 1)
+        setSubMod("bumpy", "speed", 1.2)
+        setSubMod("hidden", "offset", 4)
+    end
 
     easeSetup()
 end
@@ -19,7 +21,8 @@ function modSetup()
     startMod("opX", "XModifier", "opponent", -1)
     startMod("opZ", "ZModifier", "opponent", -1)
     startMod("stealthOP", "StealthModifier", "opponent", -1)
-    startMod('orotate', 'StrumLineRotateModifier', 'opponent', -1)
+    startMod('oAngle', 'StrumAngleModifier', 'opponent', -1)
+    startMod("orotate", "StrumLineRotateModifier", "opponent", -1)
     startMod('osplit', 'SplitModifier', 'opponent', -1)
     startMod("opInv", "InvertModifier", "opponent", -1)
     startMod('opReverse', "ReverseModifier", "opponent", -1)
@@ -36,7 +39,8 @@ function modSetup()
     startMod("bfTipsy", "TipsyYModifier", "player", -1)
     startMod("bfDrunk", "DrunkYModifier", "player", -1)
     startMod('psplit', 'SplitModifier', 'player', -1)
-    startMod('protate', 'StrumLineRotateModifier', 'player', -1)
+    startMod('pAngle', 'StrumAngleModifier', 'player', -1)
+    startMod("protate", "StrumLineRotateModifier", "player", -1)
     startMod("bfX", "XModifier", "player", -1)
     startMod("bfInv", "InvertModifier", "player", -1)
     startMod('bfReverse', "ReverseModifier", "player", -1)
@@ -73,9 +77,11 @@ function easeSetup()
     ease(0, 16, "quadOut", [[
         0, opY
     ]])
-    ease(0, 16, "linear", [[
-        1, hidden
-    ]])
+    if HardMode then
+        ease(0, 16, "linear", [[
+            1, hidden
+        ]])
+    end
     ease(16, 16, "quadOut", [[
         0, bfY,
         0, opSwap,
@@ -95,9 +101,6 @@ function easeSetup()
     ]])
     ease(32, 2, "linear", [[
         1, boost
-    ]])
-    set(32, [[
-        1, wiggle
     ]])
 
     for i, v in pairs(bass(16)) do
@@ -123,21 +126,37 @@ function easeSetup()
             ease(time + 1, 1.25, 'quadOut', string.format("0, curveX, 0, %srotate:y", char))
             --ease(time + 1, 1.25, 'quadOut', string.format("0, opX, 0, bfX"))
         end
+
+        if not EasyMode then
+            for i = 1, 2 do
+                local index = (beat%2==0 and 0 or 2)+i
+                if (beat%4<2) then
+                    set(time, [[
+                        0, ]] .. char .. [[Angle]].. index .. [[
+                    ]])
+                end
+                ease(time, 1, "linear", [[
+                    ]] .. (beat%4 >= 2 and 360 or 180) .. [[, ]]..char..[[Angle]]..index..[[
+                ]])
+            end
+        end
     end
 
-    ease(36, 1,"quadOut", "1, opInv")
-    ease(37, 1,"quadOut", "0, opInv")
-    ease(38, 1,"quadOut", "1, opFlip")
-    ease(39, 1,"quadOut", "0, opFlip")
-    ease(44, 2,"linear", "1, opReverse")
-    ease(46, 2,"linear", "0, opReverse")
+    if HardMode then
+        ease(36, 1,"quadOut", "1, opInv")
+        ease(37, 1,"quadOut", "0, opInv")
+        ease(38, 1,"quadOut", "1, opFlip")
+        ease(39, 1,"quadOut", "0, opFlip")
+        ease(44, 2,"linear", "1, opReverse")
+        ease(46, 2,"linear", "0, opReverse")
 
-    ease(52, 1,"quadOut", "1, bfInv")
-    ease(53, 1,"quadOut", "0, bfInv")
-    ease(54, 1,"quadOut", "1, bfFlip")
-    ease(55, 1,"quadOut", "0, bfFlip")
-    ease(60, 2,"linear", "1, bfReverse")
-    ease(62, 2,"linear", "0, bfReverse")
+        ease(52, 1,"quadOut", "1, bfInv")
+        ease(53, 1,"quadOut", "0, bfInv")
+        ease(54, 1,"quadOut", "1, bfFlip")
+        ease(55, 1,"quadOut", "0, bfFlip")
+        ease(60, 2,"linear", "1, bfReverse")
+        ease(62, 2,"linear", "0, bfReverse")
+    end
 
     ease(46, 2, "expoInOut", [[
         0, bfZ,
@@ -160,7 +179,7 @@ function easeSetup()
 
     for beat = 0, (4*8-1) do
         local time = 64 + beat
-        local turn = beat >= 16 and "op" or "bf"
+        local turn = beat >= 16 and "bf" or "op"
         local inten = 2*(beat%2==0 and 1 or -1)
         ease(time, 1, 'sineIn', [[
             ]]..(beat%2==0 and -150 or 125)..[[, opZ,
@@ -173,44 +192,59 @@ function easeSetup()
         set(time, (time%2==0 and 600 or -600) ..' ,curveX, 1, brake')
         ease(time, 1, 'quadOut', '0 ,curveX, 0, brake')
 
-        set(time, [[
-        ]] .. inten .. [[,]] .. turn .. [[Tipsy,
-        ]] .. inten .. [[,]] .. turn .. [[Drunk
-        ]])
-        ease(time, 1, 'quadOut', [[
-            0, ]] .. turn .. [[Tipsy,
-            0, ]] .. turn .. [[Drunk
-        ]])
+        if not EasyMode then
+            set(time, [[
+            ]] .. inten .. [[,]] .. turn .. [[Tipsy,
+            ]] .. inten .. [[,]] .. turn .. [[Drunk
+            ]])
+            ease(time, 1, 'quadOut', [[
+                0, ]] .. turn .. [[Tipsy,
+                0, ]] .. turn .. [[Drunk
+            ]])
+        end
     end
-    --bf side
-    ease(64, 1, "linear", "1, bfHid"); ease(64, 2, "linear", "1, bfWiggle")
-    ease(64, 1, "quadOut", "-0.1, bfFlip"); ease(64+1, 1, "cubeOut", "1, bfFlip")
-    ease(68, 1, "quadOut", "1.1, bfFlip"); ease(68+1, 1, "cubeOut", "0, bfFlip")
-    ease(72, 1, "quadOut", "-0.1, bfFlip"); ease(72+1, 1, "cubeOut", "1, bfFlip")
-    ease(76, 1, "quadOut", "1.1, bfFlip"); ease(76+1, 1, "quadOut", "0, bfFlip")
-    ease(76, 1, "quadOut", "-0.1, bfInv"); ease(76+1, 1, "cubeOut", "1, bfInv")
-    ease(78, 1, "quadOut", "1.1, bfInv"); ease(78+1, 1, "cubeOut", "0, bfInv")
-    ease(64, 1, "linear", "1, bfHid"); ease(64, 2, "linear", "1, bfWiggle")
-
-    --dad side
-    ease(80, 1, "linear", "1, opHid"); ease(80, 2, "linear", "1, opWiggle")
-    ease(80, 1, "quadOut", "-0.1, opFlip"); ease(80+1, 1, "cubeOut", "1, opFlip")
-    ease(84, 1, "quadOut", "1.1, opFlip"); ease(84+1, 1, "cubeOut", "0, opFlip")
-    ease(88, 1, "quadOut", "-0.1, opFlip"); ease(88+1, 1, "cubeOut", "1, opFlip")
-    ease(92, 1, "quadOut", "1.1, opFlip"); ease(92+1, 1, "quadOut", "0, opFlip")
-    ease(92, 1, "quadOut", "-0.1, opInv"); ease(92+1, 1, "cubeOut", "1, opInv")
-    ease(94, 1, "quadOut", "1.1, opInv"); ease(94+1, 1, "cubeOut", "0, opInv")
-    ease(94, 1, "linear", "1, opHid"); ease(94, 2, "linear", "1, opWiggle")
-
-    for i = 0, 1 do
-        local add = i * 16
-        ease(65 + add, 2, "quadOut", "360, angle")
-        ease(69 + add, 2, "quadOut", "0, angle")
-        ease(73 + add, 2, "quadOut", "360, angle")
-        ease(77 + add, 2, "quadOut", "0, angle")
-        ease(79 + add, i == 1 and 1 or 2, "quadOut", "360, angle")
+    if HardMode then
+        for beat = 0, 8-1 do
+            local time = 64 + (beat*4)
+            ease(time, 1, 'sineOut', '45, localRotate:x')
+            ease(time+1, 1, 'sineIn', '0, localrotate:x')
+            ease(time+2, 1, 'sineOut', '-45, localRotate:x')
+            ease(time+3, 1, 'sineIn', '0, localrotate:x')
+        end
     end
-    set(96, "0, angle")
+    if HardMode then
+        --bf side
+        ease(64, 1, "linear", "1, bfHid"); ease(64, 2, "linear", "1, bfWiggle")
+        ease(64, 1, "quadOut", "-0.1, bfFlip"); ease(64+1, 1, "cubeOut", "1, bfFlip")
+        ease(68, 1, "quadOut", "1.1, bfFlip"); ease(68+1, 1, "cubeOut", "0, bfFlip")
+        ease(72, 1, "quadOut", "-0.1, bfFlip"); ease(72+1, 1, "cubeOut", "1, bfFlip")
+        ease(76, 1, "quadOut", "1.1, bfFlip"); ease(76+1, 1, "quadOut", "0, bfFlip")
+        ease(76, 1, "quadOut", "-0.1, bfInv"); ease(76+1, 1, "cubeOut", "1, bfInv")
+        ease(78, 1, "quadOut", "1.1, bfInv"); ease(78+1, 1, "cubeOut", "0, bfInv")
+        ease(78, 1, "linear", "1, bfHid"); ease(78, 2, "linear", "0, bfWiggle")
+
+        --dad side
+        ease(80, 1, "linear", "1, opHid"); ease(80, 2, "linear", "1, opWiggle")
+        ease(80, 1, "quadOut", "-0.1, opFlip"); ease(80+1, 1, "cubeOut", "1, opFlip")
+        ease(84, 1, "quadOut", "1.1, opFlip"); ease(84+1, 1, "cubeOut", "0, opFlip")
+        ease(88, 1, "quadOut", "-0.1, opFlip"); ease(88+1, 1, "cubeOut", "1, opFlip")
+        ease(92, 1, "quadOut", "1.1, opFlip"); ease(92+1, 1, "quadOut", "0, opFlip")
+        ease(92, 1, "quadOut", "-0.1, opInv"); ease(92+1, 1, "cubeOut", "1, opInv")
+        ease(94, 1, "quadOut", "1.1, opInv"); ease(94+1, 1, "cubeOut", "0, opInv")
+        ease(94, 1, "linear", "1, opHid"); ease(94, 2, "linear", "0, opWiggle")
+    end
+
+    if HardMode then
+        for i = 0, 1 do
+            local add = i * 16
+            ease(65 + add, 2, "quadOut", "360, angle")
+            ease(69 + add, 2, "quadOut", "0, angle")
+            ease(73 + add, 2, "quadOut", "360, angle")
+            ease(77 + add, 2, "quadOut", "0, angle")
+            ease(79 + add, i == 1 and 1 or 2, "quadOut", "360, angle")
+        end
+        set(96, "0, angle, 0, localRotate:x")
+    end
     ease(96, 1, "sineInOut", "0.5, tipZ")
     ease(95, 1, "sineInOut", [[
         0.5, opSwap,
@@ -218,14 +252,16 @@ function easeSetup()
         0.5, stealthBF
     ]])
 
-    for i = 0, 1 do
-        local add = i * 16
-        local turn = i==0 and "op" or "bf"
-        ease(96+add, 1, "linear", [[1, ]] .. turn .. [[Reverse]])
-        ease(100+add, 1, "linear", [[0, ]] .. turn .. [[Reverse]])
-        ease(104+add, 1, "linear", [[1, ]] .. turn .. [[Reverse]])
-        ease(109+add, 1, "linear", [[0, ]] .. turn .. [[Reverse]])
-        ease(111+add, 1, "linear", [[0.5, ]] .. turn .. [[Reverse]])
+    if not EasyMode then
+        for i = 0, 1 do
+            local add = i * 16
+            local turn = i==0 and "op" or "bf"
+            ease(96+add, 1, "linear", [[1, ]] .. turn .. [[Reverse]])
+            ease(100+add, 1, "linear", [[0, ]] .. turn .. [[Reverse]])
+            ease(104+add, 1, "linear", [[1, ]] .. turn .. [[Reverse]])
+            ease(109+add, 1, "linear", [[0, ]] .. turn .. [[Reverse]])
+            ease(111+add, 1, "linear", [[0.5, ]] .. turn .. [[Reverse]])
+        end
     end
     for beat = 0, (4*8)-1 do
         local time = 96 + beat
@@ -283,18 +319,20 @@ function easeSetup()
             ]] .. 3 * (beat%2==0 and 1 or -1) .. [[, opDrunk
         ]])
         ease(time, 1, "quadOut", "0, bfDrunk, 0, opDrunk")
-        if beat%4 == 0 then
-            ease(time, 1 ,'quadOut', '20, protate:y')
-            ease(time, 1 ,'quadOut', '-20, orotate:y')
-        elseif beat%4 == 1 then
-            ease(time, 1 ,'quadOut', '0, protate:y')
-            ease(time, 1 ,'quadOut', '0, orotate:y')
-        elseif beat%4 == 2 then
-            ease(time, 1 ,'quadOut', '-20, protate:y')
-            ease(time, 1 ,'quadOut', '20, orotate:y')
-        elseif beat%4 == 3 then
-            ease(time, 1 ,'quadOut', '0, protate:y')
-            ease(time, 1 ,'quadOut', '0, orotate:y')
+        if HardMode then
+            if beat%4 == 0 then
+                ease(time, 1 ,'quadOut', '45, pAngle, -100, bfX')
+                ease(time, 1 ,'quadOut', '-45, oAngle, 100, opX')
+            elseif beat%4 == 1 then
+                ease(time, 1 ,'quadOut', '0, pAngle, 0, bfX')
+                ease(time, 1 ,'quadOut', '0, oAngle, 0, opX')
+            elseif beat%4 == 2 then
+                ease(time, 1 ,'quadOut', '-45, pAngle, 100, bfX')
+                ease(time, 1 ,'quadOut', '45, oAngle, -100, opX')
+            elseif beat%4 == 3 then
+                ease(time, 1 ,'quadOut', '0, pAngle, 0, bfX')
+                ease(time, 1 ,'quadOut', '0, oAngle, opX')
+            end
         end
     end
     ease(190, 2, "linear", [[
@@ -308,23 +346,33 @@ function easeSetup()
         0, boost,
         0.5, stealthBF,
         1, stealthOP,
-        0, protate:y,
-        0, orotate:y
+        0, protate,
+        0, orotate
     ]])
+    if HardMode then ease(190, 2, "linear", "2, bfWiggle") end
     ease(210, 4, "quadOut", "0, opY, 0, bfY")
-    ease(320, 2, "linear", "0, wiggle")
+    if HardMode then ease(320, 2, "linear", "0, bfWiggle") end
     set(320, "0, stealthOP, 0, stealthBF, 0, opSwap, 0, opTipZ, 0, opTipsy, 0, bfTipZ, 0, bfTipsy")
     for beat = 0, (4*16)-1 do
         local time = 320 + beat
         local inten = (beat%2==0 and 1 or -1)
         if time ~= 352 and time ~=368 then
-            ease(time-0.1, 0.1, "expoIn", [[
-                0.5, tipZ,
-                1, tipsy,
-                0.1, reverse,
-                ]] .. -25 * inten .. [[, x,
-                ]] .. 5 * inten .. [[, wiggle
-            ]])
+            if not EasyMode then
+                ease(time-0.1, 0.1, "expoIn", [[
+                    0.5, tipZ,
+                    1, tipsy,
+                    0.1, reverse,
+                    ]] .. -25 * inten .. [[, x,
+                    ]] .. 5 * inten .. [[, wiggle
+                ]])
+            else
+                ease(time-0.1, 0.1, "expoIn", [[
+                    0.5, tipZ,
+                    1, tipsy,
+                    0.1, reverse,
+                    ]] .. -25 * inten .. [[, x
+                ]])
+            end
             ease(time, 0.9, "quadOut", [[
                 0, tipZ,
                 0, tipsy,
@@ -334,18 +382,20 @@ function easeSetup()
             ]])
         end
     end
-    for beat = 0, (4*8)-1 do
-        local time = 384 + beat
-        local inten = 2*(beat%2==0 and 0.5 or -0.5)
+    if HardMode then
+        for beat = 0, (4*8)-1 do
+            local time = 384 + beat
+            local inten = 2*(beat%2==0 and 0.5 or -0.5)
 
-        set(time, [[
-        ]] .. inten .. [[, tipsy,
-        ]] .. inten .. [[, drunk
-        ]])
-        ease(time, 1, 'quadOut', [[
-            0, tipsy,
-            0, drunk
-        ]])
+            set(time, [[
+            ]] .. inten .. [[, tipsy,
+            ]] .. inten .. [[, drunk
+            ]])
+            ease(time, 1, 'quadOut', [[
+                0, tipsy,
+                0, drunk
+            ]])
+        end
     end
     for beat = 0, (4*8)-1 do
         local time = 416 + beat
